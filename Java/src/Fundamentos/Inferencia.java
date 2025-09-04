85afa7ba171da4128bd48ee5bb84ac3b9399342d
raoní silla <Raonisilla@gmail.com>
2025-09-04 15:51:42 -0300

    update

---
+++ Java/src/Fundamentos/Inferencia.java
@@ -0,0 +1,27 @@
+package Fundamentos;
+
+public class Inferencia {
+    public static void main(String[] args) {
+        double a = 4.5;
+        System.out.println(a);
+
+        var b = 4.5; // o java vai inferir pelo tipo da variavel e valor colocado qual é o tipo da variavel
+        System.out.println(b);
+        var c = "Texto";
+        System.out.println(c);
+        c = "new_text";
+        System.out.println(c);
+
+        // quando se tem uma inferencia não pode se mudar o valor, sempre sendo de um determinado tipo e esse nunca ira mudar
+       // c = 7; isso da erro, porque o java ja entendeu que a variavel "c" é string
+
+        double d; // variavel declarada
+        d = 123.65; // inicializada
+        System.out.println(d); // usada
+
+
+        //var e; quando se usa var, é preciso inicializar ela em uma unica linha
+        //e = 12345;
+        // inferencia é o fato que voce consegue criar variaveis usando a palavra var, porem apartir do valor definido nao pode mais ser mudado
+    }
+}
