bda46cc73b54e82b8e8779a810beeaf96b58b161
raoní silla <Raonisilla@gmail.com>
2025-09-09 12:07:17 -0300

    update

---
+++ Java/src/Estrututra_De_Controle/Break2.java
@@ -0,0 +1,16 @@
+package Estrututra_De_Controle;
+
+public class Break2 {
+    public static void main(String[] args) {
+        //break rotulado
+      externo:  for (int i = 0; i < 3; i ++){
+           interno: for (int j = 0 ; j < 3; j++){ // deu um nome pros dois laços
+                if (i == 1){
+                   // break; esse break age apenas no modulo mais interno
+                    break externo;
+
+                }
+            }
+        }
+    }
+}
