a13ef0e381e6b83dd64d7f75a65f562fa3c628dc
raoní silla <Raonisilla@gmail.com>
2025-09-09 11:01:19 -0300

    update

---
+++ Java/src/Desafios/DesafioFor2.java
@@ -0,0 +1,13 @@
+package Desafios;
+
+public class DesafioFor2 {
+    public static void main(String[] args) {
+        final int T = 0;
+
+        for (int i = 10; i >= T; i -= 2){ // aqui eu decremento de 2 em 2
+
+            System.out.println("O indice vale: "+  i);
+
+        }
+    }
+}
