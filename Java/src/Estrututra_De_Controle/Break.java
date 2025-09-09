9a5d18b89c6b0e0da798e39a05d9311575d8246d
raoní silla <Raonisilla@gmail.com>
2025-09-09 11:53:42 -0300

    update

---
+++ Java/src/Estrututra_De_Controle/Break.java
@@ -0,0 +1,15 @@
+package Estrututra_De_Controle;
+
+public class Break {
+    public static void main(String[] args)
+    {   int i;
+        for ( i = 0; i < 10; i++){
+            if (i==5){
+                break; //quebra o fluxo naquele momento, saindo da estrutura atual, nao fazendo sentido dentro de um if, e sim dentro de um laço
+
+            }
+        }
+        System.out.println(i);
+    }
+
+}
