57de8edd6a970b32499dc690697e01bfaa6b345e
raoní silla <Raonisilla@gmail.com>
2025-09-10 13:36:55 -0300

    update

--- Java/src/Classe/Data.java
+++ Java/src/Classe/Data.java
@@ -5,22 +5,28 @@ public class Data {
     int mes;
     int ano;
 
-    Data(int diaInicial, int mesInicial, int anoInicial){
-
-        dia = diaInicial;
-        mes = mesInicial;
-        ano = anoInicial;
+    Data(int dia, int mes, int ano){
+        // aqui eu digo oque são os parametros do construtor  e oque são variaveis do objeto que vai ser criado
+        //todos que tem o this, vao ser variaveis do objeto que vai ser criado apartir desse construtor e em frente são os parametros
+        //que vao ser passados a essas variaveis
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
 
     }
     Data(){
-        dia = 01; // dia padrao
-        mes = 01; //mes padrao
-        ano = 1970; //ano padrao
-    }
+       // dia = 01; // dia padrao
+       // mes = 01; //mes padrao
+       // ano = 1970; //ano padrao
 
+        this (01,01,1970); // posso usar o this como objeto e como metodo
+        //oque vai definir qual construtor o this vai chamar, so podendo usar this como metodo dentro do construtor
+        //são os parametros passados
+    }
+    // como nao é um metodo estatico e sim um metodo de instancia, eu posso usar o this para referenciar o objeto atual
     String obterDataFormatada (){
 
-        return String.format("%d/%d/%d", dia, mes, ano); //o metodo format formata como se fosse um printf
+        return String.format("%d/%d/%d", this.dia, this.mes, this.ano); //o metodo format formata como se fosse um printf
 
     }
 }
