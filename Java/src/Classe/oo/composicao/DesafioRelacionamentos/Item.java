42609360370c9655bc02b0401a0fd320b474d187
raoní silla <Raonisilla@gmail.com>
2025-09-16 13:58:09 -0300

    update

--- Java/src/Classe/oo/composicao/DesafioRelacionamentos/Item.java
+++ Java/src/Classe/oo/composicao/DesafioRelacionamentos/Item.java
@@ -1,4 +1,11 @@
 package Classe.oo.composicao.DesafioRelacionamentos;
 
 public class Item {
+   final int qntd;
+    final Produto produto;
+
+    Item (Produto produto, int qntd){
+        this.qntd = qntd;
+        this.produto = produto;
+    }
 }
--- Java/src/Classe/oo/composicao/DesafioRelacionamentos/Produto.java
+++ Java/src/Classe/oo/composicao/DesafioRelacionamentos/Produto.java
@@ -1,4 +1,13 @@
 package Classe.oo.composicao.DesafioRelacionamentos;
 
+import java.util.ArrayList;
+
 public class Produto {
+    String nome;
+    double preco;
+
+    Produto (String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
 }
