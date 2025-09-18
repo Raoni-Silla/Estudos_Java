f3f51f6f5304eef3d6879c0ffa43c3ac704e2ac0
raoní silla <Raonisilla@gmail.com>
2025-09-18 11:57:59 -0300

    update

--- Java/src/Classe/oo/composicao/OOheranca/Heroi.java
+++ Java/src/Classe/oo/composicao/OOheranca/Heroi.java
@@ -1,4 +1,17 @@
 package Classe.oo.composicao.OOheranca;
 
 public class Heroi extends Jogador {
+
+    boolean atacar (Jogador jogador){
+
+
+        //aqui eu chamo o metodo da classe pai(superclasse)
+        boolean atq1 = super.atacar(jogador);
+        boolean atq2 = super.atacar(jogador);
+        boolean atq3 = super.atacar(jogador);
+        boolean atq4 = super.atacar(jogador);
+
+        return atq1 || atq2 || atq3 || atq4;
+    }
+
 }
--- Java/src/Classe/oo/composicao/OOheranca/Jogo.java
+++ Java/src/Classe/oo/composicao/OOheranca/Jogo.java
@@ -4,21 +4,21 @@ public class Jogo {
 
     public static void main(String[] args) {
 
-        Jogador j1 = new Heroi();
+        Jogador heroi = new Heroi();
         Jogador j2 = new Jogador();
-        j1.x = 10;
-        j1.y = 10;
+        heroi.x = 10;
+        heroi.y = 10;
 
         j2.x = 10;
         j2.y = 11;
 
-        System.out.println(j1.life);
-        System.out.println(j2.life);
+        System.out.println("hp hero: " + heroi.life);
+        System.out.println("hp jogador " + j2.life);
 
-        j1.atacar(j2);
-        j2.atacar(j1);
+        heroi.atacar(j2);
+        j2.atacar(heroi);
 
-        System.out.println(j1.life);
+        System.out.println(heroi.life);
         System.out.println(j2.life);
 
 
