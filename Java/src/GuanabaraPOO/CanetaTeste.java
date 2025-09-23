29cf666f0e6a02cceb0a073e97b605f743a93fe9
raoní silla <Raonisilla@gmail.com>
2025-09-23 13:07:59 -0300

    update

--- Java/src/GuanabaraPOO/CanetaTeste.java
+++ Java/src/GuanabaraPOO/CanetaTeste.java
@@ -4,6 +4,7 @@ public class CanetaTeste {
     public static void main(String[] args) {
 
         Caneta c1 = new Caneta("Bic", 0.15);
+        //chamo o get para pegar o nome do modelo e da ponta
         System.out.println(c1.getPoint());
         System.out.println(c1.getModel());
         c1.status();
