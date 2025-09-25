7ea2d4840556f6e6441b23d6c9523b02543e31a7
raoní silla <Raonisilla@gmail.com>
2025-09-25 16:41:26 -0300

    Update

--- Java/src/GuanabaraPOO/UFC/Lutas.java
+++ Java/src/GuanabaraPOO/UFC/Lutas.java
@@ -1,5 +1,7 @@
 package GuanabaraPOO.UFC;
 
+import java.util.Random;
+
 public class Lutas {
 
     private Lutador desafiado;
@@ -10,20 +12,49 @@ public class Lutas {
     public void marcarLuta(Lutador l1,Lutador l2){
 
 
-        if (l1.getCategorias().equalsIgnoreCase(l2.getCategorias())){
+        if (l1.getCategorias().equalsIgnoreCase(l2.getCategorias()) && l1 != l2){
 
+            this.setAprovado(true);
+            this.desafiado = l1;
+            this.desafiante = l2;
 
-
+        }else {
+            this.setAprovado(false);
+            this.desafiado = null;
+            this.desafiante = null;
         }
 
-        this.desafiado = l1;
-        this.desafiante = l2;
+
 
 
 
     }
 
     public void lutar(){
+
+        if (isAprovado()){
+            Random gerador = new Random();
+            this.desafiado.apresentar();
+            this.desafiante.apresentar();
+            int vencedor = gerador.nextInt(0,2);
+            switch (vencedor){
+                case 0:
+                    System.out.println("Empate");
+                    this.desafiado.empatarLuta();
+                    this.desafiante.empatarLuta();
+                    break;
+                case 1:
+                    System.out.println("vitoria do desafiado");
+                    this.desafiado.ganharLuta();
+                    this.desafiante.perderLuta();
+                    break;
+                case 2:
+                    System.out.println("vitoria do desafiante");
+                    this.desafiante.ganharLuta();
+                    this.desafiado.perderLuta();
+                    break;
+            }
+        }else System.out.println("luta nao pode acontecer");
 
 
 
