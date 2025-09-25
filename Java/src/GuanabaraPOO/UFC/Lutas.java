9d0d11421f7c10c00e54aa49d167507304fee984
raoní silla <Raonisilla@gmail.com>
2025-09-25 15:50:32 -0300

    Update

--- Java/src/GuanabaraPOO/UFC/Lutas.java
+++ Java/src/GuanabaraPOO/UFC/Lutas.java
@@ -7,11 +7,49 @@ public class Lutas {
     private int rounds;
     private boolean aprovado;
 
-    public void marcarLuta(){
+    public void marcarLuta(Lutador l1){
+
+        this.desafiado = l1;
+
 
     }
 
     public void lutar(){
 
+
+
+    }
+
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public boolean isAprovado() {
+        return aprovado;
+    }
+
+    public void setAprovado(boolean aprovado) {
+        this.aprovado = aprovado;
     }
 }
