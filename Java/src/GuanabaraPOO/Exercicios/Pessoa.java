048d37061d7710e4a16813ac1e66d63a1ce8b203
raoní silla <Raonisilla@gmail.com>
2025-09-26 16:12:29 -0300

    update

---
+++ Java/src/GuanabaraPOO/Exercicios/Pessoa.java
@@ -0,0 +1,47 @@
+package GuanabaraPOO.Exercicios;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+
+
+    Pessoa(String nome, int idade, String sexo) {
+        this.setNome(nome);
+        this.setIdade(idade);
+        this.setSexo(sexo);
+    }
+
+    public void fazerAniversario(){
+        this.setIdade(this.getIdade() + 1);
+    }
+
+
+
+    //-------------------getters e setters----------------------------//
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
