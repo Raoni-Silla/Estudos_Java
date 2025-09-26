eaf21a40fe370d51d40e9a9a94768e5a81514e89
raoní silla <Raonisilla@gmail.com>
2025-09-26 16:19:51 -0300

    update

---
+++ Java/src/GuanabaraPOO/Exercicios/BibliotecaGua/Publicacao.java
@@ -0,0 +1,10 @@
+package GuanabaraPOO.Exercicios.BibliotecaGua;
+
+public interface Publicacao {
+
+    public void abrir();
+    public void fechar();
+    public void folhear();
+    public void avancarPag();
+    public void voltarPag();
+}
