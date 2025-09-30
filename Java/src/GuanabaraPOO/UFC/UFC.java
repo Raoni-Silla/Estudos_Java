911f337c872f1f05f2c5290aff0ba665ebefdb18
raoní silla <Raonisilla@gmail.com>
2025-09-30 12:30:17 -0300

    Update

--- Java/src/GuanabaraPOO/UFC/UFC.java
+++ Java/src/GuanabaraPOO/UFC/UFC.java
@@ -7,7 +7,7 @@ public class UFC {
         Lutador l [] = new Lutador[5];
         l[0]= new Lutador("Sukuna", "frança", 31,1.75,68.9, 11, 3 , 1 );
         l[1]= new Lutador("satoru gojo", "Brasil", 29, 1.68, 57.8, 14,2,3);
-        l[2]= new Lutador("kurapika", "EUA", 35, 1.65, 80.9, 12, 2,1);
+        l[2]= new Lutador("douma", "EUA", 35, 1.65, 80.9, 12, 2,1);
         l[3]= new Lutador("akaza", "Australia", 28, 1.93, 81.6, 13,0,2);
 
         Lutas UFC01 = new Lutas();
