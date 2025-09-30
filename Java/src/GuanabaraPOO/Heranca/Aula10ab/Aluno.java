0f5ab330791236e31acd629340a44c3b5996bec3
raoní silla <Raonisilla@gmail.com>
2025-09-30 13:00:33 -0300

    update

---
+++ Java/src/GuanabaraPOO/Heranca/Aula10ab/Aluno.java
@@ -0,0 +1,23 @@
+package GuanabaraPOO.Heranca.Aula10ab;
+
+public class Aluno {
+    private int matricula;
+    private String curso;
+
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+}
---
+++ Java/src/GuanabaraPOO/Heranca/Aula10ab/Pessoa.java
@@ -0,0 +1,38 @@
+package GuanabaraPOO.Heranca.Aula10ab;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private String sexo;
+
+
+    Pessoa(String nome, int idade, String sexo) {
+        setNome(nome);
+        setIdade(idade);
+        setSexo(sexo);
+    }
+
+    public void fazerAniversario (){
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getSexo() {
+        return sexo;
+
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
