429a0fda1dc441a14715c0478ec06eec3a5d2068
raoní silla <Raonisilla@gmail.com>
2025-10-01 16:00:06 -0300

    update

--- Java/src/Lambdas/CalculoTeste2.java
+++ Java/src/Lambdas/CalculoTeste2.java
@@ -4,7 +4,13 @@ public class CalculoTeste2 {
     public static void main(String[] args) {
 
         Calculo soma = (a,b)->{return a+b;}; //funcao anonima soma
+        //se eu coloquei corpo a função preciso do retorno
+        System.out.println(soma.executar(1,2));
 
+        soma = (x,y) -> x * y; //quando eu omito o par de chaves o
+        // java entende que tem uma unica sentença de codigo e que ele
+        // vai ser implicitamente retornado
+        //nao posso ter mais de uma linha de codigo quando nao tem chaves
         System.out.println(soma.executar(1,2));
 
     }
