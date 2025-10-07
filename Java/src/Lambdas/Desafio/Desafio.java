bfb3e6b42d69b3354c7aed44a999da9c9b9321ce
raoní silla <Raonisilla@gmail.com>
2025-10-07 11:47:52 -0300

    Update

--- Java/src/Lambdas/Desafio/Desafio.java
+++ Java/src/Lambdas/Desafio/Desafio.java
@@ -14,7 +14,19 @@ public class Desafio {
         UnaryOperator<Double> Freight = x -> x >= 3000 ? x + 100 : x + 50;
         Function <Double, Double> Arround = x -> Math.round(x * 100.0) / 100.0;
         Function <Double, String> Format = x -> x.toString().replace(".", ",");
-        System.out.println("Preço do p1 com desconto da loja R$: " + discountFunction.andThen(TaxFunction).andThen(Freight).andThen(Arround).andThen(Format).apply(p1));
+        System.out.println("Preço do p1 com desconto da loja R$: " + discountFunction
+                .andThen(TaxFunction).
+                andThen(Freight).
+                andThen(Arround).
+                andThen(Format).
+                apply(p1));
+
+        System.out.println("Preço do p2 com desconto da loja R$: " + discountFunction.
+                andThen(TaxFunction).
+                andThen(Freight).
+                andThen(Arround).
+                andThen(Format).
+                apply(p2));
 
     }
 }
