5470bacf9a0860c7de604e375097964a3964a6b5
raoní silla <Raonisilla@gmail.com>
2025-10-09 11:57:28 -0300

    Update

---
+++ Java/src/StreamAPI/Map.java
@@ -0,0 +1,29 @@
+package StreamAPI;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class Map {
+    public static void main(String[] args) {
+        Consumer<Object> println = System.out::println;
+
+        List<String> marcas = Arrays.asList("mercedes","honda","bmw","hiunday","ferrari","bid","gtr","suv","corolla");
+
+        marcas.stream(); //operação de built
+
+        marcas.stream().map(m -> m.toUpperCase()).forEach(println); //for each é termianal
+        //peguei as marcas, joguei pra maiusculo e depois imprimi
+        UnaryOperator <String> maiscula = n -> n.toUpperCase();
+        UnaryOperator <String> primeiraLetra = n -> n.charAt(0) + "";
+        UnaryOperator <String> grito = n -> n + "!!!!";
+        marcas.stream()
+                .map(maiscula)
+                .map(primeiraLetra)
+                .map(grito)
+                .forEach(println);
+
+    }
+}
