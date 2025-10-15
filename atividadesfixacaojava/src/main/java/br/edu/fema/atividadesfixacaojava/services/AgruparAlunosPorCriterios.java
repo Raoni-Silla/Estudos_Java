08255e23c2e7249a19a6a3dd569da8219cd61044
raoní silla <Raonisilla@gmail.com>
2025-10-15 15:36:16 -0300

    Update

--- atividadesfixacaojava/src/main/java/br/edu/fema/atividadesfixacaojava/services/AgruparAlunosPorCriterios.java
+++ atividadesfixacaojava/src/main/java/br/edu/fema/atividadesfixacaojava/services/AgruparAlunosPorCriterios.java
@@ -10,18 +10,62 @@ import java.time.YearMonth;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-public class AgruparAlunosPorCriterios implements Atividade{
+public class AgruparAlunosPorCriterios implements Atividade {
     @Override
     public void executar() {
         List<Aluno> listaAlunos = AlunoRepository.findAll();
-        Map<Boolean,List<Aluno>> BeneficientesEnaoBeneficientes =listaAlunos.stream().collect(Collectors.groupingBy(Aluno::getPossuiAlgumTipoDeBeneficio));
-        Map<Month, List<Aluno>> MesDataNascimento = listaAlunos.stream().collect(Collectors.groupingBy((Aluno a) -> LocalDate.parse(a.getDataNascimento(),Utilitarios.formatter).getMonth()));
-        Map<Year, List<Aluno>> AnoDataNascimento = listaAlunos.stream().collect(Collectors.groupingBy((Aluno a) -> Year.of(LocalDate.parse(a.getDataNascimento(), Utilitarios.formatter).getYear())));
-        Map<YearMonth, List<Aluno>> MesEanoNascimento = listaAlunos.stream().collect(Collectors.groupingBy((Aluno a) -> {
-            LocalDate data = LocalDate.parse(a.getDataNascimento(), Utilitarios.formatter);
-            return YearMonth.of(data.getYear(), data.getMonth());
-        }));
 
+        System.out.println("===========09-AgruparAlunosPorCriterios============");
+
+        System.out.println("\n----------- Beneficiários e não Beneficiários -----------");
+        Map<Boolean, List<Aluno>> beneficientesEnaoBeneficientes = listaAlunos.stream()
+                .collect(Collectors.groupingBy(Aluno::getPossuiAlgumTipoDeBeneficio));
+
+        beneficientesEnaoBeneficientes.forEach((beneficiario, alunos) -> {
+            String status = beneficiario ? "Beneficiários" : "Não Beneficiários";
+            System.out.println("\n--- Grupo: " + status + " ---");
+            alunos.forEach(aluno -> System.out.println("  - " + aluno.getNomeCompleto()));
+        });
+
+
+        System.out.println("\n----------- Mês de Nascimento -----------");
+        Map<Month, List<Aluno>> mesDataNascimento = listaAlunos.stream()
+                .collect(Collectors.groupingBy(aluno -> LocalDate.parse(aluno.getDataNascimento(), Utilitarios.formatter).getMonth()));
+
+        mesDataNascimento.forEach((mes, alunos) -> {
+            System.out.println("\n--- Nascidos em: " + mes + " ---");
+            alunos.forEach(aluno -> System.out.println("  - " + aluno.getNomeCompleto()));
+        });
+
+
+        System.out.println("\n----------- Ano de Nascimento -----------");
+        Map<Year, List<Aluno>> anoDataNascimento = listaAlunos.stream()
+                .collect(Collectors.groupingBy(aluno -> Year.from(LocalDate.parse(aluno.getDataNascimento(), Utilitarios.formatter))));
+
+        anoDataNascimento.forEach((ano, alunos) -> {
+            System.out.println("\n--- Nascidos em: " + ano + " ---");
+            alunos.forEach(aluno -> System.out.println("  - " + aluno.getNomeCompleto()));
+        });
+
+
+        System.out.println("\n----------- Mês e Ano de Nascimento -----------");
+        Map<YearMonth, List<Aluno>> mesEanoNascimento = listaAlunos.stream()
+                .collect(Collectors.groupingBy(aluno -> YearMonth.from(LocalDate.parse(aluno.getDataNascimento(), Utilitarios.formatter))));
+
+        mesEanoNascimento.forEach((anoMes, alunos) -> {
+            System.out.println("\n--- Nascidos em: " + anoMes + " ---");
+            alunos.forEach(aluno -> System.out.println("  - " + aluno.getNomeCompleto()));
+        });
+
+
+        System.out.println("\n----------- Possuem Curso ou Não -----------");
+        Map<Boolean, List<Aluno>> possueCursoOuNao = listaAlunos.stream()
+                .collect(Collectors.groupingBy(aluno -> aluno.getCurso() != null));
+
+        possueCursoOuNao.forEach((possuiCurso, alunos) -> {
+            String status = possuiCurso ? "POSSUEM" : "NÃO POSSUEM";
+            System.out.println("\n--- Alunos que " + status + " curso ---");
+            alunos.forEach(aluno -> System.out.println("  - " + aluno.getNomeCompleto()));
+        });
     }
 }
