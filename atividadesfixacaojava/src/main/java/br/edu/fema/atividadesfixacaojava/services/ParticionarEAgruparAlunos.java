62a9553d6c6995ffd836d5c33d5d5e0efb6f9904
raoní silla <Raonisilla@gmail.com>
2025-10-15 16:18:57 -0300

    Update

--- atividadesfixacaojava/src/main/java/br/edu/fema/atividadesfixacaojava/services/ParticionarEAgruparAlunos.java
+++ atividadesfixacaojava/src/main/java/br/edu/fema/atividadesfixacaojava/services/ParticionarEAgruparAlunos.java
@@ -1,8 +1,33 @@
 package br.edu.fema.atividadesfixacaojava.services;
 
+import br.edu.fema.atividadesfixacaojava.model.Aluno;
+import br.edu.fema.atividadesfixacaojava.repository.AlunoRepository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class ParticionarEAgruparAlunos implements Atividade{
     @Override
     public void executar() {
+        List<Aluno> listaAlunos = AlunoRepository.findAll();
+
+        Map<Boolean, Map<String, List<Aluno>>> parte1 = listaAlunos.stream().collect(
+                Collectors.partitioningBy(
+                        Aluno::getPossuiAlgumTipoDeBeneficio,
+                        Collectors.groupingBy(
+                                aluno -> {
+                                    if (aluno.getCurso() != null) {
+                                        return aluno.getCurso().getSigla();
+                                    } else {
+                                        return "SEM CURSO";
+                                    }
+                                }
+                        )
+                )
+        );
+
+
 
     }
 }
