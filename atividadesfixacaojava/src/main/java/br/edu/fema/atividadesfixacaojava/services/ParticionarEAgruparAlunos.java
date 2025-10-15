eb91733aa5e8afee34c17c16ba56425cf000e190
raoní silla <Raonisilla@gmail.com>
2025-10-15 16:48:05 -0300

    Update

--- atividadesfixacaojava/src/main/java/br/edu/fema/atividadesfixacaojava/services/ParticionarEAgruparAlunos.java
+++ atividadesfixacaojava/src/main/java/br/edu/fema/atividadesfixacaojava/services/ParticionarEAgruparAlunos.java
@@ -1,5 +1,6 @@
 package br.edu.fema.atividadesfixacaojava.services;
 
+import br.edu.fema.atividadesfixacaojava.enums.Periodo;
 import br.edu.fema.atividadesfixacaojava.model.Aluno;
 import br.edu.fema.atividadesfixacaojava.repository.AlunoRepository;
 
@@ -7,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class ParticionarEAgruparAlunos implements Atividade{
+public class ParticionarEAgruparAlunos implements Atividade {
     @Override
     public void executar() {
         List<Aluno> listaAlunos = AlunoRepository.findAll();
@@ -27,7 +28,17 @@ public class ParticionarEAgruparAlunos implements Atividade{
                 )
         );
 
+        Map<Boolean, List<Aluno>> alunosSeparados = listaAlunos.stream().collect(Collectors.partitioningBy(a -> a.getCurso() != null && a.getCurso().getPeriodo() != null));
+        List<Aluno> validos = alunosSeparados.get(true);
+        List<Aluno> invalidos = alunosSeparados.get(false);
 
-
+        Map<Boolean, Map<Periodo, Map<String, List<Aluno>>>> listaParticionados = validos.stream().collect(Collectors.partitioningBy(
+                        Aluno::getPossuiAlgumTipoDeBeneficio,
+                        Collectors.groupingBy(a ->
+                                a.getCurso().getPeriodo(),
+                                Collectors.groupingBy(a -> a.getCurso().getSigla())
+                          )
+                        )
+                     );
     }
 }
