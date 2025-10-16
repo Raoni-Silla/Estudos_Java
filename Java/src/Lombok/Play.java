2a4bd9b7f8f5cdc2d5656d0ba116f2b9d80c99ca
raoní silla <Raonisilla@gmail.com>
2025-10-16 16:47:53 -0300

    Update

--- Java/src/Lombok/Play.java
+++ Java/src/Lombok/Play.java
@@ -3,8 +3,7 @@ package Lombok;
 public class Play {
     public static void main(String[] args) {
         Treino02 aluno = new Treino02();
-        aluno.setNome("Aluno");
-        aluno.setDescricao("descricao");
+
 
         System.out.println("Aluno: " + aluno);
     }
--- Java/src/Lombok/Treino02.java
+++ Java/src/Lombok/Treino02.java
@@ -1,20 +1,20 @@
 package Lombok;
 
-
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode (onlyExplicitlyIncluded = true)
+@RequiredArgsConstructor
+@AllArgsConstructor //um construtor com todos os argumentos
 public class Treino02 {
 
     private String nome;
 
     @ToString.Exclude
-    @Setter(value = AccessLevel.PRIVATE)
+    //@Setter(value = AccessLevel.PRIVATE)
+    @EqualsAndHashCode.Include
     private long id;
 
     private String descricao;
