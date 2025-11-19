d20c602e99a09e95bfb4627918894a9c23666a42
Raoni Silla <Raonisilla@gmail.com>
2025-11-19 13:58:01 -0300

    Update

---
+++ src/main/java/br/com/dicasdeumdev/apitestesunitarios/ApiTestesUnitariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.apitestesunitarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestesUnitariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestesUnitariosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dicasdeumdev/apitestesunitarios/ApiTestesUnitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.apitestesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesUnitariosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
